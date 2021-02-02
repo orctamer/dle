46aef039ed93f99ee67ae62ff5807d7bac81a6ee
orctamer <efrenpalacios2011@live.com>
2021-02-01 17:30:03 -0800

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
         <main>
             <img src={image} alt="Logo Banner" className="w-full" />
             <section className="bg-blue-200 min-h-screen p-12">
-                <div className="container mx-auto"><h1>Welcome to Duel Links Entertainment!</h1></div>
+                <div className="container mx-auto"><h1>Welcome to Duel Links Entertainment</h1></div>
             </section>
         </main>
     )
